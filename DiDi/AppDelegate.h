e52d72097d4c0a3b0cf93201a3b36a120ea09930
李昊宇 <lhy_enterprise@163.com>
2019-06-17 16:25:45 +0800

    Initial Commit

---
+++ DiDi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiDi
+//
+//  Created by gcks on 2019/6/17.
+//  Copyright © 2019 gcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiDi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiDi
+//
+//  Created by gcks on 2019/6/17.
+//  Copyright © 2019 gcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
